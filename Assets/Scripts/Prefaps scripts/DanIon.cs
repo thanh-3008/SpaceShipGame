25be83bd19c63e3262654391a403d522d4412bab
Thành <mrsthanh180183@gmail.com>
2025-09-28 22:39:20 +0700

    [ThanhNTQE]/Skill đạn ion cho tàu Aegis và thiết lập lại độ khó cho game

---
+++ Assets/Scripts/Prefaps scripts/DanIon.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class DanIon : MonoBehaviour
+{   
+    private Rigidbody2D rb;
+    public float speed = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        if (transform.position.y > 6f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            PlayerController player = FindObjectOfType<PlayerController>();
+            
+                other.GetComponent<thienthachdichuyen>().TakeDame(player.damehientai*200);
+           
+        }
+    }
+}
+
---
+++ Assets/Scripts/Spawn scripts/SpawnDanIon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SpawnDanIon : MonoBehaviour
+{
+    // Kéo các đối tượng vào đây trong Inspector
+    public GameObject DanIon;
+    public PlayerController player;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Spawn();
+        }
+    }
+
+    public void Spawn()
+    {
+        // Không cần GetComponent nữa vì đã có tham chiếu trực tiếp
+        if (player == null)
+        {
+            Debug.LogError("Chưa gán Player vào script SpawnTauMe!");
+            return;
+        }
+
+        // LỖI LOGIC Ở ĐÂY: Phải so sánh Nộ với Nộ, không phải Nộ với Máu
+        if (player.thanhNoHienTai >= player.thanhNoToiDa )
+        {
+            // Tạo dan ion
+            Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            Instantiate(DanIon, spawnPosition, Quaternion.identity);
+
+            // Reset thanh nộ
+            player.thanhNoHienTai -= 100f;          
+        }
+    }
+}
