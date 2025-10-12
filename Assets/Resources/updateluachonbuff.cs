fbd4d7c1a7185f4570c3d8736403735ed9417719
thientruong <tontruongdz@gmail.com>
2025-10-12 20:12:21 +0700

    skillbosslaovao1

---
+++ Assets/Resources/updateluachonbuff.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class updateluachonbuff : MonoBehaviour
+{
+    public string upgradeName;
+    public string description;
+    public Sprite icon;
+    public System.Action onSelect;
+
+}
