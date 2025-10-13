2863eab310977ce80920d53186816304bc52c0a4
PhatMessi <nguyenluongphat1609@gmail.com>
2025-10-13 08:39:06 +0700

    Fix bug và cập nhật feature

---
+++ Assets/Scripts/Resources/updateluachonbuff.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class UpgradeOption
+{
+    public string upgradeName;
+    public string description;
+    public UnityEngine.Sprite icon; // Ghi rõ UnityEngine.Sprite để không bị nhầm lẫn
+    public System.Action onSelect;
+}
