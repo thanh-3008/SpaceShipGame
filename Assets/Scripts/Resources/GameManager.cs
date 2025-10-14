44381c733206cbb75da10789a5b39ed60efbad1a
CND <nhatdungcao@gmail.com>
2025-10-14 08:41:18 +0700

    Thêm hệ thông quản lý Game

---
+++ Assets/Scripts/Resources/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int score = 0;
+    public bool isGameOver = false;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void AddScore(int value)
+    {
+        score += value;
+        Debug.Log("Score: " + score);
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        Debug.Log("Game Over!");
+        // Hiện UI gameover
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
