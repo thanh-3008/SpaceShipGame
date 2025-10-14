7fc02f3c51798284ae699590e60a0b39d04990d8
CND <nhatdungcao@gmail.com>
2025-10-14 20:46:50 +0700

    Xử lý máu cho Player

---
+++ Assets/Scripts/Resources/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Player HP: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player chết!");
+        // TODO: thêm xử lý GameOver, UI, v.v.
+    }
+}
