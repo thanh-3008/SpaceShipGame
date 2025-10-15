ec5e8d80c5a2071f4a1b6ad6e247d56acf14640e
Raichi <vudaittk31@gmail.com>
2025-10-15 14:04:52 +0700

    [Đại] Thêm tính năng khiên cho AI Companion

---
+++ Assets/Scripts/Resources/ShieldSo25.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ShieldSo25 : MonoBehaviour
+{
+    [SerializeField]
+    public float lifetime = 8f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("EnemyBullet"))
+        {
+            Destroy(collision.gameObject); // Chặn đạn
+        }
+    }
+}
