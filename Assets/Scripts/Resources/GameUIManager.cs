3f5fc4293213ffc32fc6280268fa4a8c1266d0b5
CND <nhatdungcao@gmail.com>
2025-10-21 14:15:33 +0700

    Update file để điều khiển các panel UI

--- Assets/Scripts/Resources/GameUIManager.cs
+++ Assets/Scripts/Resources/GameUIManager.cs
@@ -15,4 +15,23 @@ public class GameUIManager : MonoBehaviour
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
     }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        if (scoreText != null)
+            scoreText.text = "Score: " + score;
+    }
+
+    public void ShowGameOver()
+    {
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(true);
+    }
+
+    public void ShowWin()
+    {
+        if (winPanel != null)
+            winPanel.SetActive(true);
+    }
 }
