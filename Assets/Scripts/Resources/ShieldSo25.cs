d7bde4e9266d97cbe72b7024967ce97b0ff3c582
VŨ VĂN ĐẠI <Daivvqe180122@fpt.edu.vn>
2025-10-22 07:34:48 +0700

    Thêm AI Companion (Trợ thủ có trí tuệ nhân tạo) và khiên

---
+++ Assets/Scripts/Resources/ShieldSo25.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ShieldSo25 : MonoBehaviour
+{
+    [SerializeField]
+    private float lifetime = 8f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("EnemyBullet"))
+        {
+            Destroy(collision.gameObject); // Chặn đạn
+        }
+    }
+}
