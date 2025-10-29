9028e01669b829f21c22cc2bf73417bddb40b042
VŨ VĂN ĐẠI <Daivvqe180122@fpt.edu.vn>
2025-10-29 07:40:19 +0700

    Heal Pack, vật phẩm hồi máu cho người chơi

---
+++ Assets/Scripts/Resources/Heal Pack 22.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class HealPack22 : MonoBehaviour
+{
+    [SerializeField]
+    private int healAmount = 30;  // Số máu hồi
+    [SerializeField]
+    private float rotationSpeed = 50f; // Quay cho đẹp
+
+    private void Update()
+    {
+        // Quay vòng tròn cho dễ thấy
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Kiểm tra nếu đối tượng chạm vào là Player
+        if (collision.CompareTag("Player"))
+        {
+            PlayerHealth22 playerHealth = collision.GetComponent<PlayerHealth22>();
+            if (playerHealth != null)
+            {
+                playerHealth.Heal(healAmount);
+            }
+
+            // Hiệu ứng & biến mất
+            Destroy(gameObject);
+        }
+    }
+}
